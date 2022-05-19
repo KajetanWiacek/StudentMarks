14976381d121419ded76eaf31a297820ab4a9026
Kajetan Wiacek <kajetanw49@gmail.com>
2022-05-19 13:24:11 +0200

    Add mark menagment functions validation

--- src/main/java/com/kajetanwiacek/studentsmarks/student/StudentService.java
+++ src/main/java/com/kajetanwiacek/studentsmarks/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.kajetanwiacek.studentsmarks.student;
 
+import com.kajetanwiacek.studentsmarks.exceptions.StudentNotFoundException;
 import com.kajetanwiacek.studentsmarks.mark.MarkMapper;
 import com.kajetanwiacek.studentsmarks.mark.MarkService;
 import com.kajetanwiacek.studentsmarks.mark.model.Mark;
@@ -28,14 +29,20 @@ public class StudentService {
     }
 
     public Student getStudent(Long studentId){
-        return studentRepo.findById(studentId).orElseThrow();
+        return studentRepo.findById(studentId).orElseThrow(() -> new StudentNotFoundException(studentId));
     }
 
     public void saveMark(Long studentId, MarkDto dto){
+        if(!studentRepo.existsById(studentId)){
+            throw new StudentNotFoundException(studentId);
+        }
         markService.saveMark(studentId,dto);
     }
 
     public List<Mark> getStudentMarks(Long studentId){
+        if(!studentRepo.existsById(studentId)){
+            throw new StudentNotFoundException(studentId);
+        }
         return markService.getStudentMarks(studentId);
     }
 }
